9ab98f9d21c85f535cef545468ff02d26575fb8e
조현지 <qndjddl001@gmail.com>
2024-11-13 16:53:18 +0900

    initial commit

---
+++ src/main/java/com/example/calenderprojectjpa/CalenderProjectJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.calenderprojectjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalenderProjectJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalenderProjectJpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/calenderprojectjpa/CalenderProjectJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.calenderprojectjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalenderProjectJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
